46005f3ffa37f7a28f616b1829947f972ea93c2f
Tom Clark <tom@octue.com>
2023-03-16 14:30:48 +0000

    OPS: Make dockerfile work with rust stack

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -5,3 +5,7 @@ poetry install
 
 # Install precommit hooks
 pre-commit install && pre-commit install -t commit-msg
+
+# Install tailwind
+#   See: https://dev.to/arctic_hen7/how-to-set-up-tailwind-css-with-yew-and-trunk-il9
+yarn global add tailwindcss
